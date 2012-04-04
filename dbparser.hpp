0e4313ccfea477cd931ec0914844845b5ab85494
Waclaw Banasik <wb305160@students.mimuw.edu.pl>
2012-04-04 16:50:43 +0200

    a

--- dbparser.hpp
+++ dbparser.hpp
@@ -5,13 +5,11 @@
 #include <istream>
 #include <list>
 #include <stdexcept>
-#include <vector>
 #include <unordered_map>
-
+#include <vector>
 
 class Target;
 class DependencyGraph;
-
 
 class CircularDependency : public std::runtime_error {
 	public:
@@ -22,6 +20,7 @@ class DependencyGraph {
 	public:
 		std::vector<Target*> main_targets;
 		std::vector<Target*> leaf_targets;
+	/*	std::vector<Target*> get_sorted_targets(); */
 
 		DependencyGraph(std::istream& ins);
 		DependencyGraph(int fd);
@@ -29,24 +28,45 @@ class DependencyGraph {
 	protected:
 		void init(std::istream& ins);
 		void topological_sort(const std::unordered_map<std::string, Target*>& targets);
-			/* throws CircularDependency */
+		/* throws CircularDependency */
 };
 
 class Target {
 	friend class DependencyGraph;
+	friend std::vector<std::vector<Target*> > get_levels(DependencyGraph graph);
+	friend void count_one_level(const std::vector<std::string>& basics, const std::string& delimiter,
+			const std::vector<Target*>& to_make, const std::vector<Target*>& not_to_make);
 	public:
-		const int id;
-		const std::string name;
+	const int id;
+	const std::string name;
 
-		void addDependency(Target* target);
+	void addDependency(Target* target);
+	std::string getCommand();
 
-		Target(const std::string& _name);
-		virtual ~Target();
+	Target(const std::string& _name);
+	virtual ~Target();
+	
+	bool realized;		/* czy juz zrobiony */
+
+	int topo_ord;		/* ile dependencies jeszcze nie zrealizowanych */
+	
+	const std::list<Target*>  & get_dependent_targets() const{
+		return dependent_targets;	
+	}
+	const std::string & get_command() const{
+		return command;
+	}
+	void set_command(const std::string &s){
+		command = s;
+	}
 	protected:
-		std::list<Target*> dependent_targets;
-		std::list<Target*> dependencies;
-		int inord;
-		static int idcounter;
+	std::list<Target*> dependent_targets;
+	std::list<Target*> dependencies;
+	std::list<std::string> needed_files;
+	int inord;
+	int outord;
+	std::string command;	/* zakladam ze to komendy rozdzielone enterami */
+	static int idcounter;
 };
 
 #endif /* _DBPARSER_ */
